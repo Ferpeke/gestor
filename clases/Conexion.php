e008287d6d14413c152586f7915c509f12437da7
FerPeke <ferna9752@gmail.com>
2021-06-30 20:16:41 -0500

    contrasenias

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -5,11 +5,10 @@
     public function conexion() {
       // $servidor = "localhost";
       // $usuario = "root";
-      // $password = "Nandodrago4";
       // $base = "gestor";
       // $puerto = "33061";
 
-      $conexion = mysqli_connect("localhost","root", "Nandodrago4", "gestor", "33061");
+      $conexion = mysqli_connect("localhost","root", "", "gestor", "33061");
       $conexion->set_charset('utf8mb4');
       return $conexion;
 
